1e82858441adc00f05c4331297f3fa5931d5f8e5
mlouis13 <mlouis@edenschool.fr>
2026-01-22 12:04:14 +0100

    repo 2

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const form = document.querySelector("#contact");
 const btn = document.querySelector("#newmodal");
 const modal = document.querySelector("#modal");
 const submit = document.querySelector("#submit");
+const section = document.querySelector("#todo");
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const data = new FormData(e.currentTarget);
 	const email = data.get("adress");
-	const doxxing = data.get("code");
-	const numero = data.get("numero");
-	console.log(email, doxxing, numero);
+
+	console.log(email);
 });
 btn.addEventListener("click", () => {
 	modal.showModal();
 });
 submit.addEventListener("click", () => {
 	modal.close();
+	const p = document.createElement("p");
+	const div = document.createElement("div");
+	div.classList.add("section");
+	p.classList.add("resultat");
+	section.appendChild(div);
+	div.appendChild(p);
+	p.textContent = "neuille";
 });
